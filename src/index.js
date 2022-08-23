a4c0cdec2898939ff06af515933ed2442146f6cd
ryan.jay.retutar <ryan.jay.retutar@oocl.com>
2022-08-23 17:20:24 +0800

    Removed mispelled words

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-funcfunction Square(props) {
+function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
         {props.value}

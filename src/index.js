67fe409571d1b42a20631db07624d9fb624e5305
ryan.jay.retutar <ryan.jay.retutar@oocl.com>
2022-08-23 17:19:07 +0800

    Implementing undoing of moves

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ function Square(props) {
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
